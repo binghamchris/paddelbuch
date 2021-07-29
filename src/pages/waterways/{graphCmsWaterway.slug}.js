6eb6c4c71567c99806193c37eaca1056fdfe127d
Chris Bingham <chris@pearit.co.uk>
2021-07-29 20:51:11 +0200

    First build-able version
    Modified how Leaflet calls are made so that production builds complete successfully
    
    Co-authored-by: Chris Bingham <3534465+binghamchris@users.noreply.github.com>

--- src/pages/waterways/{graphCmsWaterway.slug}.js
+++ src/pages/waterways/{graphCmsWaterway.slug}.js
@@ -6,9 +6,10 @@ import { graphql, Link } from "gatsby";
 import { Marker, Popup, GeoJSON } from "react-leaflet";
 import { Container, Row, Col } from "react-bootstrap";
 import L from "leaflet";
-import * as markerStyle from '../../hooks/useMarkerStyles';
+//import * as markerStyle from '../../hooks/useMarkerStyles';
 import * as layerStyle from '../../hooks/useLayerStyles';
 import { RichText } from '@graphcms/rich-text-react-renderer';
+import { isDomAvailable } from 'lib/util';
 
 export const pageQuery = graphql`
   query LakePageQuery($slug: String!) {
@@ -63,12 +64,57 @@ export const pageQuery = graphql`
 `;
 
 export default function LakeDetailsPage({ data: { graphCmsWaterway } }) {
+  var spotEinsteigAufsteigIcon
+  var spotNurEinsteigIcon
+  var spotNurAufsteigIcon
+  var spotRaststatteIcon
+  var mapSettings
 
-  const geometryL = L.geoJSON(graphCmsWaterway.geometry)
-  const mapBounds = geometryL.getBounds()
-  const mapSettings = {
-    bounds: mapBounds
-  };
+  if (isDomAvailable()) {
+    spotEinsteigAufsteigIcon = new L.icon({
+      iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+      iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",  
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+      iconAnchor: [12, 41],
+      popupAnchor: [0, -41],
+      iconSize: [25, 41],
+    })
+    
+    spotNurEinsteigIcon = new L.icon({
+      iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
+      iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",  
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+      iconAnchor: [12, 41],
+      popupAnchor: [0, -41],
+      iconSize: [25, 41],
+    })
+        
+    spotNurAufsteigIcon = new L.icon({
+      iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png",
+      iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png",  
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+      iconAnchor: [12, 41],
+      popupAnchor: [0, -41],
+      iconSize: [25, 41],
+    })
+
+    spotRaststatteIcon = new L.icon({
+      iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png",
+      iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png",  
+      shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
+      iconAnchor: [12, 41],
+      popupAnchor: [0, -41],
+      iconSize: [25, 41],
+    })
+
+    const geometryL = L.geoJSON(graphCmsWaterway.geometry)
+    const mapBounds = geometryL.getBounds()
+    mapSettings = {
+      bounds: mapBounds
+    };
+  }
+
+  
 
   return(
     
@@ -79,7 +125,7 @@ export default function LakeDetailsPage({ data: { graphCmsWaterway } }) {
       <Container fluid >
         <Row className="justify-content-center g-0">
           <Col id="map" xl="12" lg="12" md="12" sm="12" xs="12">
-            <Map {...mapSettings}>
+            <Map {...(!!mapSettings) ? mapSettings : null}>
               <GeoJSON data={graphCmsWaterway.geometry} style={layerStyle.lakeStyle}/>
 
               { graphCmsWaterway.protectedAreas.map(protectedArea => {
@@ -113,7 +159,7 @@ export default function LakeDetailsPage({ data: { graphCmsWaterway } }) {
               const { name, location, description, slug } = spot;
               const position = [location.latitude, location.longitude];
               return (
-                <Marker key={slug} position={position} icon={markerStyle.spotEinsteigAufsteigIcon}>
+                <Marker key={slug} position={position} icon={(!!spotEinsteigAufsteigIcon) ? spotEinsteigAufsteigIcon : null}>
                   {<Popup>
                     <b>{name}</b>
                     <RichText content={description.raw} />
@@ -128,7 +174,7 @@ export default function LakeDetailsPage({ data: { graphCmsWaterway } }) {
               const { name, location, description, slug } = spot;
               const position = [location.latitude, location.longitude];
               return (
-                <Marker key={slug} position={position} icon={markerStyle.spotNurEinsteigIcon}>
+                <Marker key={slug} position={position} icon={(!!spotNurEinsteigIcon) ? spotNurEinsteigIcon : null}>
                   {<Popup>
                     <b>{name}</b>
                     <RichText content={description.raw} />
@@ -143,7 +189,7 @@ export default function LakeDetailsPage({ data: { graphCmsWaterway } }) {
               const { name, location, description, slug } = spot;
               const position = [location.latitude, location.longitude];
               return (
-                <Marker key={slug} position={position} icon={markerStyle.spotNurAufsteigIcon}>
+                <Marker key={slug} position={position} icon={(!!spotNurAufsteigIcon) ? spotNurAufsteigIcon : null}>
                   {<Popup>
                     <b>{name}</b>
                     <RichText content={description.raw} />
@@ -158,7 +204,7 @@ export default function LakeDetailsPage({ data: { graphCmsWaterway } }) {
               const { name, location, description, slug } = spot;
               const position = [location.latitude, location.longitude];
               return (
-                <Marker key={slug} position={position} icon={markerStyle.spotRaststatteIcon}>
+                <Marker key={slug} position={position} icon={(!!spotRaststatteIcon) ? spotRaststatteIcon : null}>
                   {<Popup>
                     <b>{name}</b>
                     <RichText content={description.raw} />
