f435b6f6a61542f4e0f34c3340302ad1e6508600
Chris Bingham <chris@pearit.co.uk>
2021-10-20 13:08:04 +0100

    Adding Permissions-Policy to the CSP
    Adding a permission-policy to the CSP for testing with Gatsby Cloud hosting
    
    Co-authored-by: Chris Bingham <3534465+binghamchris@users.noreply.github.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,8 @@ module.exports = {
           "X-XSS-Protection: 1; mode=block",
           "X-Content-Type-Options: nosniff",
           "Referrer-Policy: strict-origin-when-cross-origin",
-          "Content-Security-Policy: default-src 'self'; img-src 'self' data: raw.githubusercontent.com api.mapbox.com; style-src 'self' 'unsafe-inline' unpkg.com; script-src 'self' 'unsafe-inline'; font-src 'self' data:"
+          "Content-Security-Policy: default-src 'self'; img-src 'self' data: raw.githubusercontent.com api.mapbox.com; style-src 'self' 'unsafe-inline' unpkg.com; script-src 'self' 'unsafe-inline'; font-src 'self' data:",
+          "Permissions-Policy: accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(self), geolocation=(self), gyroscope=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=(), clipboard-read=(), clipboard-write=(), gamepad=(), speaker-selection=(), conversion-measurement=(), focus-without-user-activation=(), hid=(), idle-detection=(), serial=(), sync-script=(), trust-token-redemption=(), vertical-scroll=*"
         ]
       }
     },
