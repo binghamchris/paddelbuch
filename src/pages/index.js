3073c1fa128962a9c76e42a0106c765690cdf0cd
Chris Bingham <chris@pearit.co.uk>
2022-02-09 20:12:50 +0100

    All Data Visible on the Index Page
    - Modified the index page so that all data is displayed on the map, rather than just spots.
    - Corrected the vertical size of full screen maps so they don't overflow at the bottom of the screen by a few pixels
    - Updated all npm packages to the latest versions
    
    Co-authored-by: Chris Bingham <3534465+binghamchris@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { Marker } from "react-leaflet";
+import { Marker, Popup, GeoJSON } from "react-leaflet";
 import Layout from "components/Layout";
 import Map from "components/Map";
 import { graphql } from "gatsby"
@@ -8,7 +8,9 @@ import { Container, Row, Col } from "react-bootstrap";
 import { isDomAvailable } from 'lib/util';
 import L from "leaflet";
 import  { markerStyles } from 'data/marker-styles';
-import { Trans, useTranslation } from 'gatsby-plugin-react-i18next';
+import { RichText } from '@graphcms/rich-text-react-renderer';
+import { Link, Trans } from 'gatsby-plugin-react-i18next';
+import * as layerStyle from 'data/layer-styles';
 
 const CH_CENTRE = {
   lat: 46.801111,
@@ -23,8 +25,8 @@ const CENTER = [CH_CENTRE.lat, CH_CENTRE.lng];
 function IndexPage ({ data }) {
 
   const spots = data.spots
-
-  const {t} = useTranslation();
+  const protectedAreas = data.protectedAreas
+  const obstacles = data.obstacles
 
   var spotEinsteigAufsteigIcon
   var spotNurEinsteigIcon
@@ -49,162 +51,123 @@ function IndexPage ({ data }) {
         <title>Swiss Paddel Buch</title>
       </Helmet>
       <Container fluid className="g-0">
-        <Row className="justify-content-center g-0">
-          <Col id="map" xl="9" lg="9" md="12" sm="12" xs="12">
+      <Row className="justify-content-center g-0">
+          <Col xl="12" lg="12" md="12" sm="12" xs="12">
             <Map {...mapSettings}>
+
             { spots.nodes
               .filter(spot => spot.spotType.slug === "einsteig-aufsteig")
               .map(spot => {
                 const { name, location, description, waterways, slug, approximateAddress, spotType } = spot;
                 const position = [location.latitude, location.longitude];
                 return (
-                  <Marker
-                    key={slug}
-                    position={position}
-                    icon={(!!spotEinsteigAufsteigIcon) ? spotEinsteigAufsteigIcon : null}
-                    eventHandlers={{
-                      click: () => {
-                        document.getElementById('welcome-message').hidden = true;
-                        document.getElementById('spot-details').hidden = false;
-                        document.getElementById('spot-name').textContent = name;
-                        document.getElementById('spot-desc').innerHTML = description.html;
-                        document.getElementById('spot-waterway').innerHTML = `<a href="./wasserlaeufe/${waterways.slug}">${waterways.name}</a>`;
-                        document.getElementById('spot-gps').textContent = location.latitude + ", " + location.longitude;
-                        document.getElementById('spot-address').textContent = approximateAddress;
-                        document.getElementById('spot-link').innerHTML = `<a href="./einsteigsorte/${slug}">` + t('More details') + `</a>`;
-                        document.getElementById('spot-type').textContent = spotType.name;
-
-                        if (description.html !== "<p>None</p>") {
-                          document.getElementById('spot-desc').innerHTML = description.html;
-                        } else {
-                          document.getElementById('spot-desc').innerHTML = "";
-                        }
-                      },
-                      
-                    }}
-                  >
+                  <Marker key={slug} position={position} icon={(!!spotEinsteigAufsteigIcon) ? spotEinsteigAufsteigIcon : null}>
+                    {<Popup>
+                      <b>{name}</b>
+                      <RichText content={description.raw} />
+                      <p><b><Trans>Type</Trans>:</b> {spotType.name}</p>
+                      <p><b><Trans>GPS</Trans>:</b> {location.latitude}, {location.longitude}</p>
+                      <p><b><Trans>Approx. Address</Trans>:</b> {approximateAddress}</p>
+                      <p><b><Trans>Waterway</Trans>:</b> {waterways.name}</p>
+                      <p><Link to={`/einsteigsorte/${slug}`}><Trans>More details</Trans></Link></p>
+                    </Popup>}
                   </Marker>
                 );
             })}
+
             { spots.nodes
               .filter(spot => spot.spotType.slug === "nur-einsteig")
               .map(spot => {
                 const { name, location, description, waterways, slug, approximateAddress, spotType } = spot;
                 const position = [location.latitude, location.longitude];
                 return (
-                  <Marker
-                    key={slug}
-                    position={position}
-                    icon={(!!spotNurEinsteigIcon) ? spotNurEinsteigIcon : null}
-                    eventHandlers={{
-                      click: () => {
-                        document.getElementById('welcome-message').hidden = true;
-                        document.getElementById('spot-details').hidden = false;
-                        document.getElementById('spot-name').textContent = name;
-                        document.getElementById('spot-desc').innerHTML = description.html;
-                        document.getElementById('spot-waterway').innerHTML = `<a href="./wasserlaeufe/${waterways.slug}">${waterways.name}</a>`;
-                        document.getElementById('spot-gps').textContent = location.latitude + ", " + location.longitude;
-                        document.getElementById('spot-address').textContent = approximateAddress;
-                        document.getElementById('spot-link').innerHTML = `<a href="./einsteigsorte/${slug}">` + t('More details') + `</a>`;
-                        document.getElementById('spot-type').textContent = spotType.name;
-
-                        if (description.html !== "<p>None</p>") {
-                          document.getElementById('spot-desc').innerHTML = description.html;
-                        } else {
-                          document.getElementById('spot-desc').innerHTML = "";
-                        }
-                      },
-                      
-                    }}
-                  >
+                  <Marker key={slug} position={position} icon={(!!spotNurEinsteigIcon) ? spotNurEinsteigIcon : null}>
+                    {<Popup>
+                      <b>{name}</b>
+                      <RichText content={description.raw} />
+                      <p><b><Trans>Type</Trans>:</b> {spotType.name}</p>
+                      <p><b><Trans>GPS</Trans>:</b> {location.latitude}, {location.longitude}</p>
+                      <p><b><Trans>Approx. Address</Trans>:</b> {approximateAddress}</p>
+                      <p><b><Trans>Waterway</Trans>:</b> {waterways.name}</p>
+                      <p><Link to={`/einsteigsorte/${slug}`}><Trans>More details</Trans></Link></p>
+                    </Popup>}
                   </Marker>
                 );
             })}
+
             { spots.nodes
               .filter(spot => spot.spotType.slug === "nur-aufsteig")
               .map(spot => {
-                const { name, location, description, waterways, slug, approximateAddress, spotType } = spot;
+                const { name, location, description, waterways, slug, approximateAddress, spotType  } = spot;
                 const position = [location.latitude, location.longitude];
                 return (
-                  <Marker
-                    key={slug}
-                    position={position}
-                    icon={(!!spotNurAufsteigIcon) ? spotNurAufsteigIcon : null}
-                    eventHandlers={{
-                      click: () => {
-                        document.getElementById('welcome-message').hidden = true;
-                        document.getElementById('spot-details').hidden = false;
-                        document.getElementById('spot-name').textContent = name;
-                        document.getElementById('spot-desc').innerHTML = description.html;
-                        document.getElementById('spot-waterway').innerHTML = `<a href="./wasserlaeufe/${waterways.slug}">${waterways.name}</a>`;
-                        document.getElementById('spot-gps').textContent = location.latitude + ", " + location.longitude;
-                        document.getElementById('spot-address').textContent = approximateAddress;
-                        document.getElementById('spot-link').innerHTML = `<a href="./einsteigsorte/${slug}">` + t('More details') + `</a>`;
-                        document.getElementById('spot-type').textContent = spotType.name;
-
-                        if (description.html !== "<p>None</p>") {
-                          document.getElementById('spot-desc').innerHTML = description.html;
-                        } else {
-                          document.getElementById('spot-desc').innerHTML = "";
-                        }
-                      },
-                      
-                    }}
-                  >
+                  <Marker key={slug} position={position} icon={(!!spotNurAufsteigIcon) ? spotNurAufsteigIcon : null}>
+                    {<Popup>
+                      <b>{name}</b>
+                      <RichText content={description.raw} />
+                      <p><b><Trans>Type</Trans>:</b> {spotType.name}</p>
+                      <p><b><Trans>GPS</Trans>:</b> {location.latitude}, {location.longitude}</p>
+                      <p><b><Trans>Approx. Address</Trans>:</b> {approximateAddress}</p>
+                      <p><b><Trans>Waterway</Trans>:</b> {waterways.name}</p>
+                      <p><Link to={`/einsteigsorte/${slug}`}><Trans>More details</Trans></Link></p>
+                    </Popup>}
                   </Marker>
                 );
             })}
+
             { spots.nodes
               .filter(spot => spot.spotType.slug === "rasthalte")
               .map(spot => {
-                const { name, location, description, waterways, slug, approximateAddress, spotType } = spot;
+                const { name, location, description, waterways, slug, approximateAddress, spotType  } = spot;
                 const position = [location.latitude, location.longitude];
                 return (
-                  <Marker
-                    key={slug}
-                    position={position}
-                    icon={(!!spotRasthalteIcon) ? spotRasthalteIcon : null}
-                    eventHandlers={{
-                      click: () => {
-                        document.getElementById('welcome-message').hidden = true;
-                        document.getElementById('spot-details').hidden = false;
-                        document.getElementById('spot-name').textContent = name;
-                        document.getElementById('spot-desc').innerHTML = description.html;
-                        document.getElementById('spot-waterway').innerHTML = `<a href="./wasserlaeufe/${waterways.slug}">${waterways.name}</a>`;
-                        document.getElementById('spot-gps').textContent = location.latitude + ", " + location.longitude;
-                        document.getElementById('spot-address').textContent = approximateAddress;
-                        document.getElementById('spot-link').innerHTML = `<a href="./einsteigsorte/${slug}">` + t('More details') + `</a>`;
-                        document.getElementById('spot-type').textContent = spotType.name;
-
-                        if (description.html !== "<p>None</p>") {
-                          document.getElementById('spot-desc').innerHTML = description.html;
-                        } else {
-                          document.getElementById('spot-desc').innerHTML = "";
-                        }
-                      },
-                      
-                    }}
-                  >
+                  <Marker key={slug} position={position} icon={(!!spotRasthalteIcon) ? spotRasthalteIcon : null}>
+                    {<Popup>
+                      <b>{name}</b>
+                      <RichText content={description.raw} />
+                      <p><b><Trans>Type</Trans>:</b> {spotType.name}</p>
+                      <p><b><Trans>GPS</Trans>:</b> {location.latitude}, {location.longitude}</p>
+                      <p><b><Trans>Approx. Address</Trans>:</b> {approximateAddress}</p>
+                      <p><b><Trans>Waterway</Trans>:</b> {waterways.name}</p>
+                    <p><Link to={`/einsteigsorte/${slug}`}><Trans>More details</Trans></Link></p>
+                    </Popup>}
                   </Marker>
                 );
             })}
+
+            { protectedAreas.nodes
+              .map(protectedArea => {
+              const { name, geometry, protectedAreaType } = protectedArea;
+              return (
+                <GeoJSON data={geometry} style={layerStyle.protectedAreaStyle}>
+                  <Popup>
+                    <b>{name}</b>
+                    <br />{protectedAreaType.name}
+                  </Popup>
+                </GeoJSON>
+              )              
+            })}
+            
+            { obstacles.nodes
+              .map(obstacle => {
+              const { name, geometry, obstacleType, portageRoute, slug } = obstacle;
+              return (
+                <div>
+                  <GeoJSON data={geometry} style={layerStyle.obstacleStyle}>
+                    <Popup>
+                      <b>{name}</b>
+                      <br />{obstacleType.name}
+                      <p><Link to={`/hindernisse/${slug}`}><Trans>More details</Trans></Link></p>
+                    </Popup>
+                  </GeoJSON>
+                  <GeoJSON data={portageRoute} style={layerStyle.portageStyle}>
+                    <Popup><b><Trans>Portage route for</Trans> {name}</b></Popup>
+                  </GeoJSON>
+                </div>
+              )            
+              })}
+
             </Map>
-          </Col>
-          <Col xl="3" lg="3" md="12" sm="12" xs="12">
-            <div className="info-pane">
-              <div id="welcome-message">
-               <p><Trans>Please click a spot for information</Trans></p>
-              </div>
-              <div id="spot-details" hidden={true}>
-                <h1 id="spot-name"> </h1>
-                <span id="spot-desc"></span>
-                <p><b><Trans>Type</Trans>:</b> <span id="spot-type"></span></p>
-                <p><b><Trans>GPS</Trans>:</b> <span id="spot-gps"></span></p>
-                <p><b><Trans>Approx. Address</Trans>:</b> <span id="spot-address"></span></p>
-                <p><b><Trans>Waterway</Trans>:</b> <span id="spot-waterway"></span></p>
-                <p><span id="spot-link"></span></p>
-              </div>
-            </div>
           </Col>
         </Row>
       
@@ -252,5 +215,35 @@ export const pageQuery = graphql`
         slug
       }
     }
+    protectedAreas: allGraphCmsProtectedArea(filter: {locale: {eq: $language}}) {
+      nodes {
+        name
+        geometry
+        slug
+        protectedAreaType {
+          name
+          slug
+        }
+        isAreaMarked
+      }
+    }
+    obstacles: allGraphCmsObstacle(filter: {locale: {eq: $language}}) {
+      nodes {
+        slug
+        portageRoute
+        geometry
+        name
+        description {
+          raw
+        }
+        isPortageNecessary
+        isPortagePossible
+        obstacleType {
+          name
+          slug
+        }
+      }
+    }
   }
 `;
+
