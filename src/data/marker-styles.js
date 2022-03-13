f5a7859508ecef4ec03f0ebd6939b50695eec61d
Chris Bingham <chris@pearit.co.uk>
2022-03-13 18:40:49 +0100

    Switched to Custom Marker Icons
    Added custom marker icons, using the new colour scheme, and  replaced the generic icons with them
    
    Co-authored-by: Chris Bingham <3534465+binghamchris@users.noreply.github.com>

--- src/data/marker-styles.js
+++ src/data/marker-styles.js
@@ -1,39 +1,50 @@
+import EinsteigAufsteigMarker2x from "assets/images/markers/marker-2x-startingspots-entryexit.png";
+import EinsteigMarker2x from "assets/images/markers/marker-2x-startingspots-entry.png";
+import AufsteigMarker2x from "assets/images/markers/marker-2x-otherspots-exit.png";
+import NotauswasserungMarker2x from "assets/images/markers/marker-2x-otherspots-emergency.png";
+import RasthalteMarker2x from "assets/images/markers/marker-2x-otherspots-rest.png";
+import EinsteigAufsteigMarker from "assets/images/markers/marker-startingspots-entryexit.png";
+import EinsteigMarker from "assets/images/markers/marker-startingspots-entry.png";
+import AufsteigMarker from "assets/images/markers/marker-otherspots-exit.png";
+import NotauswasserungMarker from "assets/images/markers/marker-otherspots-emergency.png";
+import RasthalteMarker from "assets/images/markers/marker-otherspots-rest.png";
+
 export const markerStyles = {
   spotEinsteigAufsteigIcon: {
-    iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",  
+    iconRetinaUrl: EinsteigAufsteigMarker2x,
+    iconUrl: EinsteigAufsteigMarker,  
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
     iconAnchor: [12, 41],
     popupAnchor: [0, -41],
     iconSize: [25, 41],
   },
   spotNurEinsteigIcon: {
-  iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
-  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",  
+  iconRetinaUrl: EinsteigMarker2x,
+  iconUrl: EinsteigMarker,  
   shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
   iconAnchor: [12, 41],
   popupAnchor: [0, -41],
   iconSize: [25, 41],
   },
   spotNurAufsteigIcon: {
-    iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png",
-    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png",  
+    iconRetinaUrl: AufsteigMarker2x,
+    iconUrl: AufsteigMarker,  
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
     iconAnchor: [12, 41],
     popupAnchor: [0, -41],
     iconSize: [25, 41],
   },
   spotRasthalteIcon: {
-    iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png",
-    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png",  
+    iconRetinaUrl: RasthalteMarker2x,
+    iconUrl: RasthalteMarker,  
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
     iconAnchor: [12, 41],
     popupAnchor: [0, -41],
     iconSize: [25, 41],
   },
   spotNotauswasserungIcon: {
-    iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png",
-    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png",  
+    iconRetinaUrl: NotauswasserungMarker2x,
+    iconUrl: NotauswasserungMarker,  
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
     iconAnchor: [12, 41],
     popupAnchor: [0, -41],
