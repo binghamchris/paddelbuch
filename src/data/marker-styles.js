ce34a501698c484be22924803bb3103a86b48e46
Chris Bingham <chris@pearit.co.uk>
2022-03-19 12:38:43 +0100

    Converted Marker Icons to SVG
    Converted all map marker icons from PNG to SVG to make scaling and management easier in the future.
    Minor code clean up.

--- src/data/marker-styles.js
+++ src/data/marker-styles.js
@@ -1,61 +1,57 @@
-import EinsteigAufsteigMarker2x from "assets/images/markers/marker-2x-startingspots-entryexit.png";
-import EinsteigMarker2x from "assets/images/markers/marker-2x-startingspots-entry.png";
-import AufsteigMarker2x from "assets/images/markers/marker-2x-otherspots-exit.png";
-import NotauswasserungMarker2x from "assets/images/markers/marker-2x-otherspots-emergency.png";
-import RasthalteMarker2x from "assets/images/markers/marker-2x-otherspots-rest.png";
-import EinsteigAufsteigMarker from "assets/images/markers/marker-startingspots-entryexit.png";
-import EinsteigMarker from "assets/images/markers/marker-startingspots-entry.png";
-import AufsteigMarker from "assets/images/markers/marker-otherspots-exit.png";
-import NotauswasserungMarker from "assets/images/markers/marker-otherspots-emergency.png";
-import RasthalteMarker from "assets/images/markers/marker-otherspots-rest.png";
+import EinsteigAufsteigMarker from "assets/images/markers/startingspots-entryexit.svg";
+import EinsteigMarker from "assets/images/markers/startingspots-entry.svg";
+import AufsteigMarker from "assets/images/markers/otherspots-exit.svg";
+import NotauswasserungMarker from "assets/images/markers/otherspots-emergency.svg";
+import RasthalteMarker from "assets/images/markers/otherspots-rest.svg";
+
 
 export const markerStyles = {
   spotEinsteigAufsteigIcon: {
-    iconRetinaUrl: EinsteigAufsteigMarker2x,
+    iconRetinaUrl: EinsteigAufsteigMarker,
     iconUrl: EinsteigAufsteigMarker,  
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
-    iconAnchor: [12, 41],
-    popupAnchor: [0, -41],
-    iconSize: [25, 41],
+    iconAnchor: [16, 53],
+    popupAnchor: [0, -53],
+    iconSize: [32, 53],
   },
   spotNurEinsteigIcon: {
-  iconRetinaUrl: EinsteigMarker2x,
+  iconRetinaUrl: EinsteigMarker,
   iconUrl: EinsteigMarker,  
   shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
-  iconAnchor: [12, 41],
-  popupAnchor: [0, -41],
-  iconSize: [25, 41],
+  iconAnchor: [16, 53],
+  popupAnchor: [0, -53],
+  iconSize: [32, 53],
   },
   spotNurAufsteigIcon: {
-    iconRetinaUrl: AufsteigMarker2x,
+    iconRetinaUrl: AufsteigMarker,
     iconUrl: AufsteigMarker,  
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
-    iconAnchor: [12, 41],
-    popupAnchor: [0, -41],
-    iconSize: [25, 41],
+    iconAnchor: [16, 53],
+    popupAnchor: [0, -53],
+    iconSize: [32, 53],
   },
   spotRasthalteIcon: {
-    iconRetinaUrl: RasthalteMarker2x,
+    iconRetinaUrl: RasthalteMarker,
     iconUrl: RasthalteMarker,  
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
-    iconAnchor: [12, 41],
-    popupAnchor: [0, -41],
-    iconSize: [25, 41],
+    iconAnchor: [16, 53],
+    popupAnchor: [0, -53],
+    iconSize: [32, 53],
   },
   spotNotauswasserungIcon: {
-    iconRetinaUrl: NotauswasserungMarker2x,
+    iconRetinaUrl: NotauswasserungMarker,
     iconUrl: NotauswasserungMarker,  
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
-    iconAnchor: [12, 41],
-    popupAnchor: [0, -41],
-    iconSize: [25, 41],
+    iconAnchor: [16, 53],
+    popupAnchor: [0, -53],
+    iconSize: [32, 53],
   },
   obstacleIcon: {
     iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
     iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",  
     shadowUrl: require("leaflet/dist/images/marker-shadow.png").default,
-    iconAnchor: [12, 41],
-    popupAnchor: [0, -41],
-    iconSize: [25, 41],
+    iconAnchor: [16, 53],
+    popupAnchor: [0, -53],
+    iconSize: [32, 53],
   }
 }
