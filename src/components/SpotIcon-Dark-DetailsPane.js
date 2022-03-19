ee13b9d8ad83487608e33e1078dd857c7e29a561
Chris Bingham <chris@pearit.co.uk>
2022-03-19 10:42:22 +0100

    Changed Dark Spot Icons to SVGs
    Switched the dark spot type icons from raster files to build-time rendering from SVGs.
    
    Co-authored-by: Chris Bingham <3534465+binghamchris@users.noreply.github.com>

--- src/components/SpotIcon-Dark-DetailsPane.js
+++ src/components/SpotIcon-Dark-DetailsPane.js
@@ -2,19 +2,19 @@ import React from "react";
 import { useTranslation } from 'gatsby-plugin-react-i18next';
 import { StaticImage } from "gatsby-plugin-image";
 
-const SpotIconLightPopup = (props) => {
+const SpotIconDarkDetailsPane = (props) => {
 
   const {t} = useTranslation();
   
   if (props.slug === "einsteig-aufsteig") {
     return (
-      <div class="popup-icon-div">
+      <div class="spot-icon-div">
         <p>
           <StaticImage
-            src="../assets/images/icons/entryexit-light.svg"
+            src="../assets/images/icons/entryexit-dark.svg"
             alt={t('Entry and exit spot icon')}           
             height="20"
-            className="popup-icon"
+            className="spot-icon"
           />
           {props.name}
         </p>
@@ -23,13 +23,13 @@ const SpotIconLightPopup = (props) => {
   }
   else if (props.slug === "nur-einsteig") {
     return (
-      <div class="popup-icon-div">
+      <div class="spot-icon-div">
         <p>
           <StaticImage
-            src="../assets/images/icons/entry-light.svg"
+            src="../assets/images/icons/entry-dark.svg"
             alt={t('Entry spot icon')}           
             height="20"
-            className="popup-icon"
+            className="spot-icon"
           />
           {props.name}
         </p>
@@ -39,13 +39,13 @@ const SpotIconLightPopup = (props) => {
 
   else if (props.slug === "nur-aufsteig") {
     return (
-      <div class="popup-icon-div">
+      <div class="spot-icon-div">
         <p>
           <StaticImage
-            src="../assets/images/icons/exit-light.svg"
+            src="../assets/images/icons/exit-dark.svg"
             alt={t('Exit spot icon')}           
             height="20"
-            className="popup-icon"
+            className="spot-icon"
           />
           {props.name}
         </p>
@@ -55,13 +55,13 @@ const SpotIconLightPopup = (props) => {
 
   else if (props.slug === "notauswasserungsstelle") {
     return (
-      <div class="popup-icon-div">
+      <div class="spot-icon-div">
         <p>
           <StaticImage
-            src="../assets/images/icons/emergency-light.svg"
+            src="../assets/images/icons/emergency-dark.svg"
             alt={t('Emergency exit spot icon')}           
             height="20"
-            className="popup-icon"
+            className="spot-icon"
           />
           {props.name}
         </p>
@@ -71,13 +71,13 @@ const SpotIconLightPopup = (props) => {
 
   else if (props.slug === "rasthalte") {
     return (
-      <div class="popup-icon-div">
+      <div class="spot-icon-div">
         <p>
           <StaticImage
-            src="../assets/images/icons/rest-light.svg"
+            src="../assets/images/icons/rest-dark.svg"
             alt={t('Rest spot icon')}           
             height="20"
-            className="popup-icon"
+            className="spot-icon"
           />
           {props.name}
         </p>
@@ -94,4 +94,4 @@ const SpotIconLightPopup = (props) => {
   }
 }
 
-export default SpotIconLightPopup;
+export default SpotIconDarkDetailsPane;
