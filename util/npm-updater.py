ea37f2153ac020662a07f1ebcef4c6ca9e5dd540
Chris Bingham <chris@pearit.co.uk>
2022-03-26 16:33:07 +0100

    npm Package Updates
    Updated all npm packages.
    Created a simple script to automate updating npm packages.

---
+++ util/npm-updater.py
@@ -0,0 +1,18 @@
+import json
+import subprocess as sp
+import os
+
+print("--- Updating npm Packages ---")
+outdated_pkg_json = json.loads(sp.getoutput('npm outdated --json'))
+
+for pkg in outdated_pkg_json:
+  print(f"Updating: {pkg}")
+  if pkg == "gatsby":
+    os.system('npm install gatsby@latest')
+  else:
+    os.system(f'npm update {pkg}')
+
+print("Remaining Updates (if any):")
+os.system('npm outdated')
+
+print("--- Finished ---")
