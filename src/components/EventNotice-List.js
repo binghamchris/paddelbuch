5b6bceafa15557a5fd93705f7f7d8c6250812adc
Chris Bingham <chris@pearit.co.uk>
2022-04-10 16:38:40 +0200

    Adding Waterway Event Notices
    Adding waterway event notices to the site in the following places:
    - A details page for each event notice
    - A list of active event notice (where the end date is in the future at build time) to the details page for each waterway
    - A marker and affected area for each active event notice (where the end date is in the future at build time) to the map

---
+++ src/components/EventNotice-List.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Trans, I18nextContext, Link } from 'gatsby-plugin-react-i18next';
+
+const EventNoticeList = (props) => {
+  
+  const context = React.useContext(I18nextContext);
+  const language = context.language
+
+  var endDtFormat = { year: 'numeric', month: 'long', day: 'numeric' };
+  
+  console.log(props)
+
+  console.log(props.thisWaterwayEventNotices.nodes.length)
+
+  if (props.thisWaterwayEventNotices.nodes.length > 0) {
+    return (
+      <table className="eventnotices-list">
+        <tbody>
+          <tr>
+            <th>
+              <Trans>Summary</Trans>
+            </th>
+            <th>
+              <Trans>Approx. End Date</Trans>
+            </th>
+          </tr>
+          { props.thisWaterwayEventNotices.nodes
+            .filter(waterwayEventNotice => new Date(waterwayEventNotice.endDate) - new Date() > 0)
+            .map(waterwayEventNotice => {
+              const { name, slug, endDate } = waterwayEventNotice;
+
+              var endDt
+              var endDtRaw = new Date(endDate)
+
+              if ( language === "en" ) {
+                endDt = new Intl.DateTimeFormat('en-UK', endDtFormat).format(endDtRaw)
+              }
+              if ( language === "de" ) {
+                endDt = new Intl.DateTimeFormat('de-DE', endDtFormat).format(endDtRaw)
+              }
+
+              return (
+                <tr key={slug}>
+                  <td>
+                    <Link to={`/gewaesserereignisse/${slug}`}>
+                      {name}
+                    </Link>
+                  </td>
+                  <td>{endDt}</td>
+                </tr>
+              );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+  else {
+    return (
+      <div className="eventnotices-list">
+        <Trans>There are presently no event notices for this waterway</Trans>
+      </div>
+    );
+  }
+}
+
+export default EventNoticeList;
---
+++ src/pages/gewaesserereignisse/{graphCmsWaterwayEventNotice.slug}.js
@@ -0,0 +1,122 @@
+import * as React from "react";
+import { Helmet } from "react-helmet";
+import Layout from "components/Layout";
+import Map from "components/Map-Complete";
+import { graphql } from "gatsby";
+import { Container, Row, Col } from "react-bootstrap";
+import { RichText } from '@graphcms/rich-text-react-renderer';
+import { Trans, I18nextContext, useTranslation } from 'gatsby-plugin-react-i18next';
+import { isDomAvailable } from 'lib/util';
+import L from "leaflet";
+
+export const pageQuery = graphql`
+  query WaterwayEventNoticePageQuery($slug: String!, $language: GraphCMS_Locale!) {
+    locales: allLocale(
+      filter: {language: {eq: $language}}
+    ) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+    thisNotice: graphCmsWaterwayEventNotice(locale: {eq: $language}, slug: {eq: $slug}) {
+      slug
+      updatedAt
+      name
+      location {
+        latitude
+        longitude
+      }
+      affectedArea
+      affectedSpots {
+        name
+        slug
+      }
+      affectedWaterways {
+        name
+        slug
+      }
+      description {
+        raw
+      }
+      endDate
+      startDate
+    }
+  }
+`;
+
+export default function WaterwayEventNoticeDetailsPage({ data: { thisNotice } }) {
+  const {t} = useTranslation();
+  const context = React.useContext(I18nextContext);
+  const language = context.language
+
+  var lastUpdateDtFormat = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+  var lastUpdateDtRaw = new Date(thisNotice.updatedAt)
+  var lastUpdateDt
+
+  if ( language === "en" ) {
+    lastUpdateDt = new Intl.DateTimeFormat('en-UK', lastUpdateDtFormat).format(lastUpdateDtRaw)
+  }
+  if ( language === "de" ) {
+    lastUpdateDt = new Intl.DateTimeFormat('de-DE', lastUpdateDtFormat).format(lastUpdateDtRaw)
+  }
+
+  var mapSettings
+
+  if (isDomAvailable()) {
+    const geometryL = L.geoJSON(thisNotice.affectedArea)
+    const affectedAreaBounds = geometryL.getBounds()
+    mapSettings = {
+      bounds: affectedAreaBounds,
+    };   
+  }
+
+  return(
+    <Layout pageName="notice-details">
+      <Helmet>
+        <title>{t(`Paddel Buch - Notices`)} - {thisNotice.name}</title>
+      </Helmet>
+      <Container fluid >
+        <Row className="justify-content-center g-0">
+          <Col id="map" xl="8" lg="7" md="12" sm="12" xs="12">
+            <Map {...mapSettings}>
+
+            </Map>
+          </Col>
+          <Col className="notice-description" xl="4" lg="5" md="12" sm="12" xs="12">
+            <div className="notice-title">
+              <h1>{thisNotice.name}</h1>
+            </div>
+            <RichText content={thisNotice.description.raw} />
+            <table className="notice-details-table">
+              <tbody>
+                <tr>
+                  <th><Trans>Approx. Start Date</Trans>:</th>
+                  <td>
+                    {thisNotice.startDate}
+                  </td>
+                </tr>
+                <tr>
+                  <th><Trans>Approx. End Date</Trans>:</th>
+                  <td>
+                    {thisNotice.endDate}
+                  </td>
+                </tr>
+                <tr>
+                  <th><Trans>Last Updated</Trans>:</th>
+                  <td>
+                    {lastUpdateDt}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  )
+}
