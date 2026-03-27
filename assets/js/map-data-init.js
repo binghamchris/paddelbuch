f5190be17ff6eda1d4a67009f78ab0ef00a1ea0e
Chris Bingham <binghamchris@users.noreply.github.com>
2026-03-27 18:53:24 +0100

    Add colored circle with icon to event notices layer toggle
    Create eventnotice-light.svg and eventnotice-dark.svg icons derived
    from the waterway event marker's 'i' glyph.
    
    - map-data-init.js: add icon and colorClass to eventNotices toggle
    - _filter-panel.scss: add --eventnotice modifier using $swisscanoe-blue
    
    Co-authored-by: Chris Bingham <3534465+binghamchris@users.noreply.github.com>

--- assets/js/map-data-init.js
+++ assets/js/map-data-init.js
@@ -112,7 +112,7 @@
     // Build layer toggle configuration for non-spot layers
     var layerToggles = [
       { key: 'noEntry', label: layerLabels.noEntry, layerGroup: layerGroups.noEntry, defaultChecked: false, icon: '/assets/images/icons/noentry-light.svg', colorClass: 'noentry' },
-      { key: 'eventNotices', label: layerLabels.eventNotices, layerGroup: layerGroups.eventNotices, defaultChecked: true },
+      { key: 'eventNotices', label: layerLabels.eventNotices, layerGroup: layerGroups.eventNotices, defaultChecked: true, icon: '/assets/images/icons/eventnotice-light.svg', colorClass: 'eventnotice' },
       { key: 'obstacles', label: layerLabels.obstacles, layerGroup: layerGroups.obstacles, defaultChecked: true },
       { key: 'protectedAreas', label: layerLabels.protectedAreas, layerGroup: layerGroups.protectedAreas, defaultChecked: true }
     ];
